9f95053ef6550cf93301dc64b71626e6a1cdec3b
Cherie2 <cwalk280@yahoo.com>
2022-10-22 21:49:44 -0700

    revised js for timeslots/local storage

--- assets/script.js
+++ assets/script.js
@@ -6,69 +6,72 @@ function today() {
   m = moment();
   var day=$("#currentDay");
   var toDay="";
-  toDay=(m.format( "dddd," + " MMMM Do"));
+  toDay=(m.format( "dddd," + " MMMM Do, YYYY"));
   day.append(toDay);
 }
 
 //function that creates HTMLstructure of timeslots and how they are listed
-var currentHour= moment().hour();
-//var amPm= moment().format("hA");
-//make another variable in if statement for i >12
 function createDiv() {
   var holder=$("#selectable");
-  for (let i = 9; i <= 17; i++){
+  for (let i = 9; i <= 17; i++){  
   var blockDiv=$("<div>").addClass("row time-block").attr("id", i)
   var timeDiv =$("<div>").addClass("col-1 hour").text(i)
   var textArea=$("<textarea>").addClass("col-10 description")
   var saveBtn=$("<button>").addClass("col-1 saveBtn").html("<i class='fas fa-save'>")
-  blockDiv.append(timeDiv).append(textArea).append(saveBtn)
   holder.append(blockDiv)
-  }
-  //console.log("#selectable");
+  blockDiv.append(timeDiv).append(textArea).append(saveBtn)
+  } 
+  return;
 }
-  function displayCurrentHour(hour){
-    var hour=" ";
-    if(hour<12){
+//Converts military time to standard time
+function timeAmPm(){
+  $(".hour").each(function() {
+    var hour=$(this).text();
+    if(hour < 12){
       hour+= " AM";
-    }else{
+    }else if(hour == 12){
       hour+= " PM";
-    }
-  }
+    } else {
+      hour = (hour-12) + " PM";
+    } 
+    return $(this).text(hour);
+  })
+}
 
-//It differentiates timeslots as past, current, or future based on the comparison of present time to timeslots
+var currentHour= moment().hour();
+//Function that differentiates timeslots as past, current, or future based on the comparison of present time to timeslots
 function timeBlock() {
   $( ".time-block").each(function() {
     var blockHour=$(this).attr("id");
-  if( currentHour == blockHour ) {
-    $(this).addClass("present");
-  }else if (currentHour > blockHour){
-    $(this).addClass("past");
-  } else {
+    if( currentHour == blockHour ) {
+      $(this).addClass("present");
+    }else if (currentHour > blockHour){
+      $(this).addClass("past");
+    } else {
     $(this).addClass("future");
-  }
- })
+    }
+   })
+   return;
 }
 //need to write function to allow saving of text input inside calendar
 //when page is refreshed the text should be saved on page 
-function saveTask(event) {
-    if(event === "click"){
-    $( ".description").each(function(){
-    var inputText = $(this).text();
-    var task=document.querySelector(".description");
-    inputText.innerText=task;
-    console.log(inputText);
-    localStorage.setItem("Tasks", JSON.stringify(inputText));
-    localStorage.getItem("Tasks");
-  })
-//create event listener for saveBtn, parent is timeBlock
-//time and value saved in local storage
-   }
-}
+function saveTask() {
+    $( ".row").each(function(hour,text) {
+        var hour=$(this).text();
+        var inputText = $(this).find(".description").val();
+        localStorage.setItem(hour, JSON.stringify(inputText));
+        var save = localStorage.getItem(hour, inputText); 
+    })
+    
+} 
+
 today();
 createDiv();
-displayCurrentHour();
+timeAmPm();
 timeBlock();
 saveTask();
- saveBtn=document.querySelector("textarea");
- saveBtn.addEventListener("click", saveTask);
+
+//created event listener for saveBtn 
+var saveBtn=document.querySelector("button");
+saveBtn.addEventListener("click", saveTask);
 })
